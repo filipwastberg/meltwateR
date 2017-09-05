4684838d1526848aaee30d5205d242244a4cea97
Filip Wästberg <filip.wastberg@gmail.com>
2017-09-05 16:01:47 +0200

    Update access_token.R

--- R/access_token.R
+++ R/access_token.R
@@ -1,19 +1,21 @@
 #' @title Access Meltwater token
-#' @description access_token() generates a Bearer token, which has to be renewed every hour.
+#' @description access_token() generates a Bearer token that is saved to your .Renvironment file. It is valid for 1 hour. If you run out of time, just run the function again.
 #' @param client_id your id, generated from reg_client()
 #' @param client_secret your secret, generated from reg_client
 #' @param user_key your user key generated from https://developer.meltwater.com
 #'
 #' @import httr, jsonlite
 
-access_token <- function(client_id, client_secret, user_key){
+access_token <- function(){
 token <- POST(url = "https://api.meltwater.com/oauth2/access_token",
-     authenticate(user = client_id, password = client_secret),
-     add_headers('user-key' = user_key,
+     authenticate(user = Sys.getenv("meltwater_client_id"), password = Sys.getenv("meltwater_client_secret")),
+     add_headers('user-key' =  Sys.getenv("meltwater_key"),
                  'content-type' = "application/x-www-form-urlencoded"),
      body = 'grant_type=client_credentials&scope=search')
 
 parsed_token <- jsonlite::fromJSON(content(token, "text"), simplifyVector = FALSE)
 
-return(paste("Bearer", parsed_token$access_token))
+token <- paste("Bearer", parsed_token$access_token)
+
+return(Sys.setenv("meltwater_token" = token))
 }
